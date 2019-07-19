be509c42bd6510ce25effa744e927733f16f6184
anya litvinova <anya.s.litvinova@gmail.com>
2019-07-19 20:23:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function nowServing(katzDeli){
      return "The line is currently empty.";
    } else {
      for(let num = 1; num < arr.length; num++){
-     arr = arr.split().push(i+'.').join();
+     arr = arr.join().push(i+'.')
      }
    return 'The line is currently: ' + arr;
  }
