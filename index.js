88ce8ebf034d1a5388d331cbb2af387e0c15086b
anya litvinova <anya.s.litvinova@gmail.com>
2019-07-19 20:19:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nowServing(katzDeli){
   if(katzDeli <= 0){
     return "There is nobody waiting to be served!";
   } else {
-    return 'Currently serving ' + katzDeli.shift();
+    return 'Currently serving ' + katzDeli.shift()+'.';
     }
   }
  function currentLine(arr){
