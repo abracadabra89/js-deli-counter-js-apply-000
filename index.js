beba9ef369ed08cb29524eb59b12e43e0116630c
anya litvinova <anya.s.litvinova@gmail.com>
2019-07-19 20:13:27 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,22 @@
+var katzDeli = [];
+
+function takeANumber(katzDeli, name) {
+  katzDeli.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+}
+
+function nowServing(katzDeli){
+  if(katzDeli <= 0){
+    return "There is nobody waiting to be served!";
+  } else {
+    return 'Currently serving ' + katzDeli.shift();
+    }
+  }
+ function currentLine(arr){
+   if(arr.length === 0){
+     return "The line is currently empty.";
+   } else{
+   arr = arr.join();
+   return 'The line is currently: ' + arr;
+ }
+ }
